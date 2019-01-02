6cae13c3a684de5e86f053ef15dac6d468908d4e
Thomas Taylor <thomas.taylor@kineo.com>
2019-01-02 14:08:28 +0000

    Update app start-up

--- bin/start.js
+++ bin/start.js
@@ -1,14 +1,2 @@
-const {App} = require('adapt-authoring-core');
-
-async function start() {
-  console.log(`Running application from ${process.cwd()}`);
-  const app = new App();
-  try {
-    await app.preloadDelegate(app);
-    await app.bootDelegate(app);
-  } catch(e) {
-    console.log(`Failed to start application: ${e}`);
-  }
-}
-
-module.exports = start();
+const App = require('adapt-authoring-core').App;
+App().start();
--- index.js
+++
@@ -1,7 +0,0 @@
-const config = require('adapt-authoring-config');
-
-const c = new config();
-c.preloadDelegate().then(() => {
-  console.log('then');
-  console.log('Hi', c, c.get('db'));
-});
