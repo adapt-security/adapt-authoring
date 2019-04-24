436e4ae0e6a7669e6b24d371f93008d3a6d944a6
Thomas Taylor <thomas.taylor@kineo.com>
2019-04-24 15:09:07 +0100

    Add log for convenience

--- bin/start.js
+++ bin/start.js
@@ -1,10 +1,12 @@
 const { App } = require('adapt-authoring-core');
 // TODO remove this once we have config...
-Object.assign(process.env, {
+const DEFAULTS = {
   SERVER_HOST: 'localhost',
   SERVER_PORT: 5000,
   DB_HOST: 'localhost',
   DB_PORT: 27017,
   DB_NAME: 'adapt-authoring-prototype'
-});
+};
+Object.assign(process.env, DEFAULTS);
+console.log('NOTE: for this prototype, the following env variables are assumed:\n', JSON.stringify(DEFAULTS,null,2));
 App.instance.start();
