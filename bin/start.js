640ca81985006884614627c37e481a2b732ebdae
Thomas Taylor <thomas.taylor@kineo.com>
2019-04-24 13:49:11 +0100

    Set some default env vars

--- bin/start.js
+++ bin/start.js
@@ -1,2 +1,10 @@
 const { App } = require('adapt-authoring-core');
+// TODO remove this once we have config...
+Object.assign(process.env, {
+  SERVER_HOST: 'localhost',
+  SERVER_PORT: 5000,
+  DB_HOST: 'localhost',
+  DB_PORT: 27017,
+  DB_NAME: 'adapt-authoring-prototype'
+});
 App.instance.start();
