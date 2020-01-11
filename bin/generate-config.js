8d9a4de00e6b960a12a7441f1ba8a2096ca0a04a
Thomas Taylor <thomas.taylor@kineo.com>
2020-01-11 18:15:28 +0000

    Update generate-config script in-line with new json-schema config files

--- bin/generate-config.js
+++ bin/generate-config.js
@@ -7,26 +7,36 @@ const appPkg = require(path.join(process.cwd(), 'package.json'));
 const outpath = path.resolve(path.join(process.cwd(), 'conf', `${process.env.NODE_ENV}.config.js`));
 const configJson = {};
 
-try {
-  Object.assign(configJson, require(outpath));
-  console.log(`Config already exists for NODE_ENV '${process.env.NODE_ENV}'. Any missing values will be added.`);
-} catch(e) {
-  console.log(`No config found for NODE_ENV '${process.env.NODE_ENV}'. File will be written to ${outpath}\n`);
+async function init() {
+  try {
+    Object.assign(configJson, require(outpath));
+    console.log(`Config already exists for NODE_ENV '${process.env.NODE_ENV}'. Any missing values will be added.`);
+  } catch(e) {
+    console.log(`No config found for NODE_ENV '${process.env.NODE_ENV}'. File will be written to ${outpath}\n`);
+  }
+  await Promise.all(Object.keys({ ...appPkg.dependencies, ...appPkg.devDependencies }).map(async d => {
+    const schema = await ConfigUtils.loadConfigSchema(Utils.getModuleDir(d));
+    if(!schema) {
+      return;
+    }
+    const generated = Object.entries(schema.properties).reduce((memo, [attr, config]) => {
+      memo[attr] = getValueForAttr(schema, attr);
+      return memo;
+    }, {});
+    if(Object.keys(generated).length) {
+      configJson[d] = Object.assign({}, configJson[d], generated);
+    }
+  }));
+  fs.writeFileSync(outpath, `module.exports = ${JSON.stringify(configJson, null, 2)};`);
+  console.log('Config file written successfully.\n');
+  console.log('NOTE: any null values will likely need to be specified manually.\n');
 }
-Object.keys({ ...appPkg.dependencies, ...appPkg.devDependencies }).forEach(d => {
-  const schema = ConfigUtils.loadConfigSchema(Utils.getModuleDir(d));
-  if(!schema || !schema.definition) return;
 
-  if(!configJson[d]) configJson[d] = {};
+function getValueForAttr(schema, attr) {
+  const required = schema.required && schema.required.includes(attr);
+  if(required) return null;
+  const defaultVal = schema.properties[attr].default;
+  if(defaultVal) return defaultVal;
+}
 
-  const generated = Object.entries(schema.definition).reduce((memo, [attr, config]) => {
-    if(config.default) memo[attr] = config.default;
-    else if(config.required) memo[attr] = null;
-    return memo;
-  }, {});
-
-  Object.assign(configJson[d], generated);
-});
-
-fs.writeFileSync(outpath, `module.exports = ${JSON.stringify(configJson, null, 2)};`);
-console.log('Config file written successfully.\n');
+init();
