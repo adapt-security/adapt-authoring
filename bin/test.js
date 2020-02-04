33b00f4014cd8ca3b27497aed2fe3963943eb792
Thomas Taylor <thomas.taylor@kineo.com>
2020-02-04 23:32:37 +0000

    Remove unnecessary if

--- bin/test.js
+++ bin/test.js
@@ -1,5 +1,3 @@
 const testing = require('adapt-authoring-testing');
-if(process.env.NODE_ENV !== 'testing') {
-  process.env.NODE_ENV = 'testing';
-}
+process.env.NODE_ENV = 'testing';
 module.exports = testing();
