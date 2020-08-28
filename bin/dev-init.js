4eb9bc4eda619c366314c5bae42b48c6522b853a
Tom Taylor <hello@tomtaylor.name>
2020-08-28 17:35:16 +0100

    Add global linking

--- bin/dev-init.js
+++ bin/dev-init.js
@@ -1,9 +1,37 @@
 const { exec } = require('child_process');
-const fs = require('fs-extra');
+const path = require('path');
 
-fs.readJSON('package.json', (error, pkg) => {
-  if(error) return console.log(error);
-  Object.keys({ ...pkg.dependencies, ...pkg.devDependencies }).forEach(d => {
-    exec(`npm link ${d}`, (error, stdout, stderr) => !error && !stderr && console.log(`Successfully linked '${d}'`));
-  });
+const { dependencies, devDependencies } = require('../package.json');
+const cfg = require(`../conf/${process.env.NODE_ENV}.config.js`);
+
+let localModulesDir;
+try {
+  localModulesDir = cfg['adapt-authoring-core'].modulesDir;
+} catch(e) {
+  console.log('adapt-authoring-core.localModulesDir must be set to use local modules');
+  process.exit();
+}
+Object.keys({ ...dependencies, ...devDependencies }).forEach(async d => {
+  try {
+    await globalLink(d);
+    await localLink(d);
+    console.log(`Successfully linked '${d}'`);
+  } catch {}
 });
+
+function globalLink(name) {
+  if(!localModulesDir) return Promise.resolve();
+  return new Promise((resolve, reject) => {
+    exec('npm link', { cwd: path.resolve(`${localModulesDir}/${name}`) })
+      .on('error', reject)
+      .on('exit', resolve);
+  });
+}
+
+function localLink(name) {
+  return new Promise((resolve, reject) => {
+    exec(`npm link ${name}`)
+      .on('error', reject)
+      .on('exit', resolve);
+  });
+}
