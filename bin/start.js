6a41e361045f5918bcc27d1a0d7bb44c986836b2
Thomas Taylor <thomas.taylor@kineo.com>
2022-07-04 11:16:25 +0100

    Remove dynamic import

--- bin/start.js
+++ bin/start.js
@@ -1,4 +1,4 @@
-import('adapt-authoring-core').then(({ App }) => {
-  process.env.NODE_ENV = process.env.NODE_ENV || 'production';
-  App.instance;
-});
+import { App } from 'adapt-authoring-core';
+
+process.env.NODE_ENV = process.env.NODE_ENV || 'production';
+App.instance;
