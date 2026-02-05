729881aecadbdb242db78f9f001ee39b08a783d5
Tom Taylor <thomas.taylor@kineo.com>
2026-02-05 16:08:54 +0000

    Make sure program exits on startup error

--- bin/start.js
+++ bin/start.js
@@ -1,4 +1,7 @@
 import { App } from 'adapt-authoring-core'
 
 process.env.NODE_ENV = process.env.NODE_ENV || 'production'
-App.instance.onReady().catch(e => console.error(e))
+App.instance.onReady().catch(e => {
+  console.error(e)
+  process.exit()
+})
